07f5e88f092dd4f2582a071141531168ee99ea3a
Emil Abraham <emilabraham1414@gmail.com>
2026-02-05 14:16:23 -0800

    clamp boundaries of the panel

--- player_1_script.gd
+++ player_1_script.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var screen_size
 
 func get_input():
 	var input_direction = Input.get_axis("up player 1", "down player 1")
 	velocity.y = input_direction * speed
 
+func set_paddle_boundary():
+	var paddle_half_height = 25.0
+	var child_offset_y = 250.0
+	position.y = clamp(position.y, -child_offset_y + paddle_half_height, screen_size.y - child_offset_y - paddle_half_height)
+	print("Current Position: ", position.x, ",", position.y)
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
 func _physics_process(_delta: float) -> void:
 	get_input()
 	move_and_slide()
+	set_paddle_boundary()
