0b303f21d5be5adab8ca584afee9dfdae908b1fe
Emil Abraham <emilabraham1414@gmail.com>
2026-02-05 13:37:13 -0800

    use get_axis to fix errors with get_vector

--- player_1_script.gd
+++ player_1_script.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("", "", "up player 1", "down player 1")
-	velocity = input_direction * speed
+	var input_direction = Input.get_axis("up player 1", "down player 1")
+	velocity.y = input_direction * speed
 
 func _physics_process(_delta: float) -> void:
 	get_input()
--- player_2_script.gd
+++ player_2_script.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("", "", "up player 2", "down player 2")
-	velocity = input_direction * speed
+	var input_direction = Input.get_axis("up player 2", "down player 2")
+	velocity.y = input_direction * speed
 
 func _physics_process(_delta: float) -> void:
 	get_input()
