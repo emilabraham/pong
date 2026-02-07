33c41ce950d6e566243c3ceb030aee717880dd51
Emil Abraham <emilabraham1414@gmail.com>
2026-02-06 16:54:18 -0800

    add ball direction/speed

--- ball.gd
+++ ball.gd
@@ -1,7 +1,12 @@
 extends RigidBody2D
 
+@export var speed = 200
+
 func _ready() -> void:
 	spawn_ball()
 
 func spawn_ball():
-	print("Spawn ball")
+	var angle = randf_range(-PI/4, PI/4)
+	if randi() % 2 == 0:
+		angle += PI
+	linear_velocity = Vector2.RIGHT.rotated(angle) * speed
