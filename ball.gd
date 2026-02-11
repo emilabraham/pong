77f1b70a5115b353adb3646a386794dfd08077c8
Emil Abraham <emilabraham1414@gmail.com>
2026-02-10 17:29:46 -0800

    bounce ball off paddles

--- ball.gd
+++ ball.gd
@@ -10,3 +10,8 @@ func spawn_ball():
 	if randi() % 2 == 0:
 		angle += PI
 	linear_velocity = Vector2.RIGHT.rotated(angle) * speed
+
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(linear_velocity * delta)
+	if collision:
+		linear_velocity = linear_velocity.bounce(collision.get_normal())
