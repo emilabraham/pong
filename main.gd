03c29084eec1a579afa559668c8357f7935ee6cf
Emil Abraham <emilabraham1414@gmail.com>
2026-02-11 18:05:41 -0800

    add signal for left and right boundaries

--- main.gd
+++ main.gd
@@ -9,3 +9,13 @@ func _ready() -> void:
 func new_game():
 	player_1_score = 0
 	player_2_score = 0
+
+func _on_left_boundary_body_entered(body: Node2D) -> void:
+	if body == $Ball:
+		player_2_score += 1
+		print("left body entered. Incrementing player 2 score: ", player_2_score)
+
+func _on_right_boundary_body_entered(body: Node2D) -> void:
+	if body == $Ball:
+		player_1_score += 1
+		print("right body entered. Incrementing player 1 score: ", player_1_score)
