d5bef0961898fbf61a915ea0e5568503853b4364
Emil Abraham <emilabraham1414@gmail.com>
2026-02-13 18:54:08 -0800

    add HowToPlay button

--- main_menu.gd
+++ main_menu.gd
@@ -1,8 +1,17 @@
 extends Control
 
+
+func _ready() -> void:
+	$HowToPlayCanvas.hide()
+
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_how_to_play_button_pressed() -> void:
+	$HowToPlayCanvas.show()
+
+func _on_close_button_pressed() -> void:
+	$HowToPlayCanvas.hide()
