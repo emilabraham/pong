ca1f48caa358a8f6e6bbc39b965dca8934b934e3
Emil Abraham <emilabraham1414@gmail.com>
2026-02-18 18:38:20 -0800

    connect menu button

--- main.gd
+++ main.gd
@@ -63,3 +63,7 @@ func game_over_screen() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
